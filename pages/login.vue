626d311a7bb78e0bb735fb0c9366b0613da755a2
Judith Rea Nogales <judithrea98@gmail.com>
2020-05-18 16:12:47 +0200

    Added link to register on login and created register form

--- pages/login.vue
+++ pages/login.vue
@@ -2,6 +2,7 @@
   <v-layout>
     <v-flex class="text-center">
       <h1>Login</h1>
+      <p>No account? <a href="/register">Register.</a></p>
     </v-flex>
   </v-layout>
 </template>
---
+++ pages/register.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout column align-center>
+    <h1>Register</h1>
+    <v-form>
+      <v-text-field id="full_name" v-model="full_name" label="Full Name">
+      </v-text-field>
+      <v-text-field id="nickname" v-model="nickname" label="Nickname">
+      </v-text-field>
+      <v-text-field id="email" v-model="email" label="Email"> </v-text-field>
+      <v-text-field
+        id="password"
+        v-model="password"
+        label="Password"
+      ></v-text-field>
+      <v-btn id="submit" type="submit" color="lime accent-3" class="black--text"
+        >Register</v-btn
+      >
+    </v-form>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>

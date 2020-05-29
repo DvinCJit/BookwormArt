8f01a9fc72e29c81e5697a823664acada21146f9
Judith Rea Nogales <judithrea98@gmail.com>
2020-05-30 00:54:14 +0200

    Created user schema and added pre hook to hash password

---
+++ api/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+
+const userSchema = new Schema({
+  nickname: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  token: {
+    type: String,
+    required: true
+  },
+  imageries: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Imagery'
+    }
+  ]
+})
+
+// Hash password before saving user
+userSchema.pre('save', async function(next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User

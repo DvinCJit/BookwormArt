406544639c8e65e103c407130e4bb1772ac94ec3
Judith Rea Nogales <judithrea98@gmail.com>
2020-06-13 00:07:38 +0200

    Base url config

--- services/ImageryService.js
+++ services/ImageryService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: '' || `http://localhost:3000`,
+  baseURL: process.env.BASE_URL || `http://localhost:3000`,
   withCredentials: false,
   headers: {
     'Access-Control-Allow-Origin': '*',

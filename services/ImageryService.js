9b33d95bd515c68d0b3434276abf80d475363e0c
Judith Rea Nogales <judithrea98@gmail.com>
2020-06-13 01:05:30 +0200

    Added heroku url to axios service

--- services/ImageryService.js
+++ services/ImageryService.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: '' || `http://localhost:3000`,
+  baseURL:
+    `https://bookwormart.herokuapp.com/` ||
+    process.env.BASE_URL ||
+    `http://localhost:3000`,
   withCredentials: false,
   headers: {
     'Access-Control-Allow-Origin': '*',
